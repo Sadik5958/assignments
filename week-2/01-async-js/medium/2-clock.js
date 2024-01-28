e78d68962d43eca49a154b8566dcad5d9f005ac5
Sadik5958 <mosadikhardoi@gmail.com>
2024-01-28 22:02:41 +0530

    Week2 clock.js

--- week-2/01-async-js/medium/2-clock.js
+++ week-2/01-async-js/medium/2-clock.js
@@ -8,26 +8,31 @@
 // - HH:MM::SS AM/PM (Eg 01:45:23 PM)
 
 function clock() {
-  let hh = 0;
-  let mm = 0;
-  let ss = 0;
-
   setInterval(function () {
-    ss++;
-    // console.log(ss);
+    const now = new Date();
+
+    let HH = now.getHours();
+    let MM = now.getMinutes();
+    let SS = now.getSeconds();
+
+    // Format HH, MM, SS to have leading zeros if needed
+    HH = HH < 10 ? "0" + HH : HH;
+    MM = MM < 10 ? "0" + MM : MM;
+    SS = SS < 10 ? "0" + SS : SS;
+
+    // Display time in HH:MM:SS format
+    const time24Format = `${HH}:${MM}:${SS}`;
+
+    // Convert hours to 12-hour format and determine AM/PM
+    const ampm = HH < 12 ? "AM" : "PM";
+    HH = HH % 12 || 12; // Convert 0 to 12 for 12-hour format
+
+    // Display time in HH:MM:SS AM/PM format
+    const time12Format = `${HH}:${MM}:${SS} ${ampm}`;
+
+    console.log("24-Hour Format:", time24Format);
+    console.log("12-Hour Format:", time12Format);
   }, 1000);
-
-  setInterval(function () {
-    mm++;
-    //console.log(mm);
-  }, 60000);
-
-  setInterval(function () {
-    hh++;
-    //console.log(hh);
-  }, 360000);
-
-  console.log(ss + "::" + mm + "::" + hh);
 }
 
 clock();
